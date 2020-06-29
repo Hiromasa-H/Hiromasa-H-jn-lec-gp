57d98c7c02a3ba4381541ba3fed7000580d4d682
hhiromasa <hhiromasa217@gmail.com>
2020-06-29 16:59:36 +0900

    added SQL

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from flask import Flask,redirect, url_for, render_template, request, session, flash
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "123"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False #disable warnings
 app.permanent_session_lifetime = timedelta(minutes = 15)
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    _id = db.Column("id",db.Integer,primary_key = True)
+    name = db.Column(db.String(50),unique=True)
+    email = db.Column(db.String(100),unique=True)
+
+    def __init__(self,name,email):
+        self.name = name
+        self.email = email
 
 @app.route("/")
 def home():
@@ -16,6 +29,14 @@ def login():
         session.permanent = True
         user = request.form["nm"]
         session["user"] = user
+        found_user = Users.query.filter_by(name = user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = Users(user,"") #email is blank for now
+            db.session.add(usr)
+            db.session.commit()
+
         flash(f"Login successful","info")
         return redirect(url_for("user"))
     else:
@@ -33,6 +54,9 @@ def user():
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
+            found_user = Users.query.filter_by(name = user).first()
+            found_user.email = email
+            db.session.commit()
             flash("email was saved")
         else:
             if "email" in session:
@@ -52,16 +76,11 @@ def logout():
     session.pop("email",None)
     return redirect(url_for("login"))
 
-"""
-@app.route("/<name>")
-def test(name):
-    return f"Hello {name}!"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
-"""
+@app.route("/view")
+def view():
+    return render_template("view.html", values = Users.query.all())
 
 #run app
 if __name__ == "__main__":
+    db.create_all() #create db
     app.run(debug = True) #debug = True is for debugging. 
